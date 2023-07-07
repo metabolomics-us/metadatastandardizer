a9f31cc8b9c7d26378ed1a05f0d374dffd0972d6
rictuar <parkerbremer@gmail.com>
2023-07-07 12:28:14 -0700

    pre submission update

--- frontend/code/app.py
+++ frontend/code/app.py
@@ -32,4 +32,4 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     #app.run(debug=False, host='0.0.0.0')
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)
